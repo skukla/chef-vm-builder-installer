9ff921f4065369caf824436fa9ebb383e5bd1ea4
Steve Kukla <kukla@adobe.com>
2022-05-17 11:07:21 -0400

    Updated set branch to use beta instead of dev

--- scripts/install_app.sh
+++ scripts/install_app.sh
@@ -27,11 +27,11 @@ if is_mac; then
         install_app
         echo "PROGRESS:60"
         sleep 1
-        set_branch dev
+        set_branch beta
         echo "PROGRESS:90"
         sleep 1
     else
-        set_branch dev
+        set_branch beta
         echo "PROGRESS:60"
         sleep 1
         echo "PROGRESS:70"
